ccb099c3af06bd6f8ed6668f5c9ddc8a2720894f
Danil Kostrov <kostrov@gmail.com>
2017-04-18 14:01:08 +0300

    async specs

--- src/preview.js
+++ src/preview.js
@@ -2,41 +2,121 @@ import addons from '@kadira/storybook-addons';
 import { EVENT_ID } from './';
 
 let currentStory = "";
+let asyncRows = [];
+
 const results = {};
 const beforeEachFunc = {};
 const afterFunc = {};
 const afterEachFunc = {};
 
-export function specs(specs) {
-  let storyName = specs();
-  const channel = addons.getChannel();
-  channel.emit(EVENT_ID, {results : results[storyName]});
+const ASYNC_TIMEOUT = 10000;
+
+export function specs (specs) {
+  let storyName = specs ();
+  const channel = addons.getChannel ();
+
+  if (asyncRows.length) {
+
+    channel.emit (EVENT_ID, {
+      results: results [storyName]
+    });
+
+    Promise.all (asyncRows)
+      .then (() => asyncRows = []);
+
+    asyncRows.forEach ((item) =>
+        item.then (() => channel.emit (EVENT_ID, {
+          results: results [storyName]
+        }))
+    );
+
+  } else {
+
+    channel.emit (EVENT_ID, {
+      results: results [storyName]
+    });
+
+  }
 }
 
 export const describe = (storyName, func) => {
   currentStory = storyName;
-  results[currentStory] = {
+
+  results [currentStory] = {
     goodResults: [],
     wrongResults: []
   };
 
-  func();
+  func ();
 
-  if(afterFunc[currentStory]) afterFunc[currentStory]();
+  if (afterFunc [currentStory]) {
+    afterFunc [currentStory] ();
+  }
 
   return storyName;
 };
 
-export const it = function (desc, func) {
-  if(beforeEachFunc[currentStory]) beforeEachFunc[currentStory]();
-  try {
-    func();
-    results[currentStory].goodResults.push(desc);
-  } catch (e) {
-    console.error(`${currentStory} - ${desc} : ${e}`);
-    results[currentStory].wrongResults.push({spec: desc, message: e.message});
+const updateResults = (desc, e) => {
+  if (e) {
+    console.error(`${currentStory} - ${desc} : ${e.message || e}`);
+
+    results [currentStory].wrongResults.push ({
+      spec: desc,
+      message: e.message
+    });
+  } else {
+    results [currentStory].goodResults.push (desc);
+
+    if (afterEachFunc [currentStory]) {
+      afterEachFunc [currentStory] ();
+    }
   }
-  if(afterEachFunc[currentStory]) afterEachFunc[currentStory]();
+}
+
+export const it = function (desc, func) {
+  if (beforeEachFunc [currentStory]) {
+    beforeEachFunc [currentStory] ();
+  }
+
+  if (func.length) {
+    asyncRows.push (
+      new Promise ((resolve) => {
+        let timeout = false;
+
+        let t = setTimeout (() => {
+          timeout = true;
+
+          updateResults (desc, {
+            message: `${ASYNC_TIMEOUT}ms timeout`
+          });
+
+          resolve ();
+
+        }, ASYNC_TIMEOUT);
+
+        func ((e) => {
+          clearTimeout (t);
+          updateResults (desc, e);
+
+          if (!timeout) {
+            resolve ();
+          }
+        });
+      })
+        .catch ((e) => updateResults (desc, e))
+    );
+
+  } else {
+    let e = null;
+
+    try {
+      func ();
+    } catch (err) {
+      e = err;
+    }
+
+    updateResults (desc, e);
+  }
 };
 
 export const before = function(func) {
